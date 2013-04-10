24a29a432fc1711eda8fceeaae8233fb33bebf3c
Diederik van der Boor <vdboor@edoburu.nl>
2013-04-10 14:59:37 +0200

    Add integration with django-any-urlfield

--- fluent_blogs/models/__init__.py
+++ fluent_blogs/models/__init__.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from fluent_blogs.models.db import Entry
 
 __all__ = ('Entry',)
+
+
+def _register_url_type():
+    from any_urlfield.models import AnyUrlField
+    from any_urlfield.forms.widgets import SimpleRawIdWidget
+    AnyUrlField.register_model(Entry, widget=SimpleRawIdWidget(Entry))
+
+
+if 'any_urlfield' in settings.INSTALLED_APPS:
+    _register_url_type()
