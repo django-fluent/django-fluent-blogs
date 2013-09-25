eccddb0ef43d3a97b9b9a0a0c76638d030825a3f
Diederik van der Boor <vdboor@edoburu.nl>
2013-09-25 14:29:04 +0200

    Add example apps!!

---
+++ example/example_fluent_pages/settings.py
@@ -0,0 +1,22 @@
+from os.path import join, dirname
+from example_standalone.settings import *
+
+INSTALLED_APPS += (
+    # Add fluent pages with a simple page type:
+    'fluent_pages',
+    'fluent_pages.pagetypes.flatpage',
+
+    # Add the page type for adding the "blogs" root to the page tree.
+    'fluent_blogs.pagetypes.blogpage',
+
+    # fluent-pages dependencies:
+    'mptt',
+    'polymorphic',
+    'polymorphic_tree',
+)
+
+ROOT_URLCONF = 'example_fluent_pages.urls'
+
+TEMPLATE_DIRS = (
+    join(dirname(__file__), "templates"),
+)
---
+++ example/example_standalone/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/apps/tinymce/', include('tinymce.urls')),
+    url(r'^admin/apps/tags/', include('taggit_autocomplete_modified.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^comments/', include('django.contrib.comments.urls')),
+    #url(r'^forms/', include('form_designer.urls')),
+
+    url(r'', include('fluent_blogs.urls')),
+)
