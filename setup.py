799567e60f0583b9e493ee5f4529845d2de6edfc
Diederik van der Boor <vdboor@edoburu.nl>
2013-09-25 10:23:46 +0200

    Enforce django-tag-parser >= 1.0.2 due to a fix in the assignment node.
    The 1.0.1 version has an error in the BaseAssignmentOrInclusionNode,
    where 'self' was used instead of 'cls'. This affected the `get_entries`
    tag among others.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         'django-fluent-contents>=0.8.4',
         'django-categories>=1.0.0',
-        'django-tag-parser>=1.0.0',
+        'django-tag-parser>=1.0.2',
         'django-parler>=0.9.3',
     ],
     requires=[
