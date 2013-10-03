3eb9ac1e702d89cf1f032da4d2c4b2d8755fa575
Diederik van der Boor <vdboor@edoburu.nl>
2013-10-03 07:29:47 +0200

    Fix `from fluent_blogs.models import *` import.
    Caused in AttributeError: 'module' object has no attribute
    'get_category_modelEntryManager'

--- fluent_blogs/models/__init__.py
+++ fluent_blogs/models/__init__.py
@@ -2,6 +2,6 @@ from .db import Entry, AbstractEntry, get_entry_model, get_category_model
 from .managers import EntryManager
 
 __all__ = (
-    'Entry', 'AbstractEntry', 'get_entry_model', 'get_category_model'
+    'Entry', 'AbstractEntry', 'get_entry_model', 'get_category_model',
     'EntryManager',
 )
