e466da4f26d8cbac45476e8c00e009e004cd4baa
Diederik van der Boor <vdboor@edoburu.nl>
2015-04-09 20:55:41 +0200

    Support django-contrib-comments instead of django.contrib.comments for Django 1.8

--- fluent_blogs/templatetags/fluent_blogs_comments_tags.py
+++ fluent_blogs/templatetags/fluent_blogs_comments_tags.py
@@ -6,10 +6,17 @@ when ``django.contrib.comments`` is not included in the site.
 This way, project authors can easily use an online commenting system
 (such as DISQUS or Facebook comments) instead.
 """
+from django.template import Library
+from fluent_utils.django_compat import is_installed
 
 # Expose the tag library in the site.
 # If `django.contrib.comments` is not used, this library can provide stubs instead.
 # Currently, the real tags are exposed as the template already checks for `object.comments_are_open`.
 # When a custom template is used, authors likely choose the desired commenting library instead.
 
-from django.contrib.comments.templatetags.comments import register
+if is_installed('django.contrib.comments'):
+    from django.contrib.comments.templatetags.comments import register
+elif is_installed('django_comments'):
+    from django_comments.templatetags.comments import register
+else:
+    register = Library()
