e08c2b053ab99de5a77b49b43524f5fab816b19e
Diederik van der Boor <vdboor@edoburu.nl>
2015-10-13 10:06:53 +0200

    Add render_comment_list / render_comment_form stub tags for Django

--- fluent_blogs/templatetags/fluent_blogs_comments_tags.py
+++ fluent_blogs/templatetags/fluent_blogs_comments_tags.py
@@ -6,7 +6,9 @@ when ``django.contrib.comments`` is not included in the site.
 This way, project authors can easily use an online commenting system
 (such as DISQUS or Facebook comments) instead.
 """
+import warnings
 from django.template import Library
+from django.utils.safestring import mark_safe
 from fluent_utils.django_compat import is_installed
 
 # Expose the tag library in the site.
@@ -20,3 +22,20 @@ elif is_installed('django_comments'):
     from django_comments.templatetags.comments import register
 else:
     register = Library()
+
+    @register.simple_tag
+    def render_comment_list(for_, object):
+        warnings.warn(
+            "Can't render comments list: no comment app installed!\n"
+            "Make sure either 'django.contrib.comments' or 'django_comments' is in INSTALLED_APPS"
+        )
+        return mark_safe("<!-- Can't render comments list: no comment plugin installed! -->")
+
+    @register.simple_tag
+    def render_comment_form(for_, object):
+        warnings.warn(
+            "Can't render comments form: no comment app installed!\n"
+            "Make sure either 'django.contrib.comments' or 'django_comments' is in INSTALLED_APPS"
+        )
+        return mark_safe("<!-- no comment plugin installed! -->")
+
