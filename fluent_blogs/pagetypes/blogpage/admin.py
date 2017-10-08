35f03b0348813d8961120efd5d514d25ddcdd985
Diederik van der Boor <vdboor@edoburu.nl>
2017-10-08 20:07:02 +0200

    : Perform manual BlogPageAdmin registration, as django-polymorphic 1.4+ requires.
    This new registration system worked as of django-polymorphic 1.0,
    and this deprecated feature was recently removed because it caused
    subtle bugs.

--- fluent_blogs/pagetypes/blogpage/admin.py
+++ fluent_blogs/pagetypes/blogpage/admin.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from fluent_pages.integration.fluent_contents.admin import FluentContentsPageAdmin
+from .models import BlogPage
 
 
+@admin.register(BlogPage)
 class BlogPageAdmin(FluentContentsPageAdmin):
     """
     Customized admin appearance for the blogpage model.
